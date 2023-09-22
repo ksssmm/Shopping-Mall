44fad95bffcc8ed7cd5cfd324c2bb9bbe603ef44
kim seon-min <137373182+ksssmm@users.noreply.github.com>
2023-09-22 12:03:47 +0900

    Shopping Mall

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKzYNQAoTPVTGAFc6ZVouHPe1tsUelUSk",
-  authDomain: "my-app-6c5d4.firebaseapp.com",
-  projectId: "my-app-6c5d4",
-  storageBucket: "my-app-6c5d4.appspot.com",
-  messagingSenderId: "999107842001",
-  appId: "1:999107842001:web:adc9436374947044f39af0",
-  measurementId: "G-KFH0VBM6Y1"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig); 
